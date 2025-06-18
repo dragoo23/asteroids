010a240f1d54179d18556fa31869b4fbac3241d8
dragoo23 <kaboo93@gmail.com>
2025-06-18 15:00:25 +0200

    Repository setup

---
+++ main.py
@@ -0,0 +1,10 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    return print(f"Starting Asteroids!")
+
+if __name__ == "__main__":
+    main()
